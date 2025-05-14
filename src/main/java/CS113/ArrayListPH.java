a5d307e8bb4f23945b9edfb5945e2dd335a4095a
katanu <katanu.mwendwa@gmail.com>
2025-05-13 17:26:36 -0700

    ollo

--- src/main/java/CS113/ArrayListPH.java
+++ src/main/java/CS113/ArrayListPH.java
@@ -1,157 +1,173 @@
+/**
+ * A custom implementation of an ArrayList, dynamically resizing when necessary.
+ * This class implements the ListInterface<E> interface, providing core list functionalities.
+ *
+ * @param <E> The type of elements stored in this list.
+ */
 package CS113;
-
 
 import java.util.Arrays;
 
 public class ArrayListPH<E> implements ListInterface<E> {
 
     private E[] array;
-
     private int count;
 
     final static int DEFAULT_SIZE = 10;
 
-    public ArrayListPH(E[] array) {
+    /**
+     * Constructs an empty ArrayListPH with an initial capacity of 10.
+     */
+    @SuppressWarnings("unchecked")
+    public ArrayListPH() {
         array = (E[]) new Object[DEFAULT_SIZE];
+        count = 0;
     }
 
+    /**
+     * Doubles the capacity of the array when it is full.
+     */
     private void resize() {
-        E[] resizeArray =  (E[]) new Object[array.length * 2];
-        System.arraycopy(array, 0, resizeArray, 0, resizeArray.length);
-        array = resizeArray;
+        array = Arrays.copyOf(array, array.length * 2);
     }
 
+    /**
+     * Adds an element to the end of the list.
+     * @param element The element to be added.
+     * @return true if the element was added successfully.
+     */
     @Override
     public boolean add(E element) {
-        if (array[array.length - 1] != null) {
-            int length = array.length;
-            resize();
-            array[length] = element;
-        }
-
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == null) {
-                array[i] = element;
-                return true;
-            }
-        }
+        if (count == array.length) resize();
+        array[count++] = element;
         return true;
     }
 
+    /**
+     * Adds an element at a specific index, shifting subsequent elements.
+     * @param index The index where the element should be added.
+     * @param element The element to be added.
+     */
     @Override
     public void add(int index, E element) {
-        if (index < 0 || index >= array.length) {
-            throw new IndexOutOfBoundsException();
-        }
-
-        if (array[array.length - 1] == null) {
-            resize();
-        }
-
-        if (index ==  array.length - 1) {
-            array[array.length - 1] = element;
-        }
-
-        for (int i = 0; i < array.length; i++) {
-            array[i] = array[i + 1];
-        }
-
-//        for (int i = index; i < array.length; i++) {
-////            E temp = array[i + 1];
-////            array[i + 1] = element;
-////            element = temp;
-//        }
+        if (index < 0 || index > count) throw new IndexOutOfBoundsException();
+        if (count == array.length) resize();
+        System.arraycopy(array, index, array, index + 1, count - index);
+        array[index] = element;
+        count++;
     }
 
+    /**
+     * Clears the list, removing all elements.
+     */
     @Override
     public void clear() {
         Arrays.fill(array, null);
-
-        for(E element : array) {
-            element = null;
-        }
+        count = 0;
     }
 
+    /**
+     * Checks if the list contains a specific element.
+     * @param element The element to search for.
+     * @return true if the element is found.
+     */
     @Override
     public boolean contains(Object element) {
         return indexOf(element) != -1;
     }
 
+    /**
+     * Returns the index of the first occurrence of a specified element.
+     * @param object The element to search for.
+     * @return The index of the element, or -1 if not found.
+     */
     @Override
     public int indexOf(Object object) {
-        if(object == null) {
-            return -1;
-        }
-
-        for(int i = 0; i < array.length; i++) {
-            if (object.equals(array[i])) {
+        for (int i = 0; i < count; i++) {
+            if (object == null ? array[i] == null : object.equals(array[i])) {
                 return i;
             }
         }
         return -1;
     }
 
+    /**
+     * Retrieves the element at the specified index.
+     * @param index The index of the element to retrieve.
+     * @return The element at the specified index.
+     * @throws IndexOutOfBoundsException if index is invalid.
+     */
     @Override
     public E get(int index) {
-        if (index < 0 || index >= array.length) {
-            throw new IndexOutOfBoundsException();
-        }
+        if (index < 0 || index >= count) throw new IndexOutOfBoundsException();
         return array[index];
     }
 
+    /**
+     * Checks if the list is empty.
+     * @return true if the list is empty.
+     */
     @Override
     public boolean isEmpty() {
-        return size() == 0;
+        return count == 0;
     }
 
+    /**
+     * Removes the element at the specified index.
+     * @param index The index of the element to remove.
+     * @return true if removal is successful.
+     */
     @Override
     public boolean remove(int index) {
-        for (int i = index; i < array.length - 1; i++) {
-            array[i] = array[i + 1];
-        }
-        array[array.length - 1] = null;
+        if (index < 0 || index >= count) return false;
+        System.arraycopy(array, index + 1, array, index, count - index - 1);
+        array[--count] = null;
         return true;
     }
 
+    /**
+     * Removes the first occurrence of a specified element.
+     * @param element The element to remove.
+     * @return true if the element is removed.
+     */
     @Override
     public boolean remove(E element) {
         int index = indexOf(element);
-        return remove(index);
+        return index != -1 && remove(index);
     }
 
+    /**
+     * Returns the current size of the list.
+     * @return The number of elements in the list.
+     */
     @Override
     public int size() {
-        for (int i = array.length - 1; i >= 0; i--) {
-            if (array[i] != null) {
-                return i + 1;
-            }
-        }
-        return 0;
+        return count;
     }
 
+    /**
+     * Sets the element at the specified index.
+     * @param index The index to set.
+     * @param element The new element.
+     */
     @Override
     public void set(int index, E element) {
-        if (index < 0 || index >= array.length) {
-            throw new IndexOutOfBoundsException();
-        }
+        if (index < 0 || index >= count) throw new IndexOutOfBoundsException();
         array[index] = element;
     }
 
+    /**
+     * Returns a string representation of the list.
+     * @return A string representation of the list.
+     */
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-
-        int size = size();
-
-        for (int i = 0; i < size; i++) {
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < count; i++) {
             sb.append(array[i]);
-
-            if (i != size - 1) {
-                sb.append(", ");
-            }
+            if (i < count - 1) sb.append(", ");
         }
-
-        sb.append("[");
+        sb.append("]");
         return sb.toString();
     }
 }
--- src/main/java/CS113/Main.java
+++ src/main/java/CS113/Main.java
@@ -2,6 +2,50 @@ package CS113;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Create an instance of ArrayListPH
+        ArrayListPH<Integer> intList = new ArrayListPH<>();
+
+        // Test: Add elements
+        System.out.println("Adding integers to the list:");
+        intList.add(10);
+        intList.add(20);
+        intList.add(30);
+        System.out.println("List after additions: " + intList);
+
+        // Test: Add element at specific index
+        intList.add(1, 15);
+        System.out.println("\nList after adding 15 at index 1: " + intList);
+
+        // Test: Access elements
+        System.out.println("\nElement at index 2: " + intList.get(2));
+
+        // Test: Remove element by index
+        intList.remove(1);  // Removing 15
+        System.out.println("\nList after removing element at index 1: " + intList);
+
+        // Test: Remove element by value
+        intList.remove(Integer.valueOf(30)); // Removing value 30
+        System.out.println("\nList after removing 30: " + intList);
+
+        // Test: Check size
+        System.out.println("\nSize of list: " + intList.size());
+
+        // Test: Clear the list
+        intList.clear();
+        System.out.println("\nList after clear: " + intList);
+        System.out.println("\nSize of list after clear: " + intList.size());
+
+        // Test: Check if the list is empty
+        System.out.println("\nIs the list empty? " + intList.isEmpty());
+
+        // Add some elements again and use other types like String
+        ArrayListPH<String> strList = new ArrayListPH<>();
+        strList.add("Hello");
+        strList.add("There");
+        System.out.println("\nString list after additions: " + strList);
+
+        // Test: Contains
+        System.out.println("\nDoes the list contain 'Hello'? " + strList.contains("Hello"));
+        System.out.println("Does the list contain 'General Kenobi'? " + strList.contains("General Kenobi"));
     }
 }
